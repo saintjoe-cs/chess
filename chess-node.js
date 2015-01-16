b6ed8323d34ea08e6e555bcb3831466aae40aae0
Brian Capouch <brianc@saintjoe.edu>
2015-01-16 03:26:09 -0500

    Clean up spacing and string delimiters

--- chess-node.js
+++ chess-node.js
@@ -1,10 +1,10 @@
 // Your code here.
-var hash = "#",
+var hash = '#',
     space = ' ',
     size = 8;
 
 for (var row = 0; row < size; row++) {
   for (var col = 0; col < size; col++) 
     process.stdout.write(col%2 == 0?space:hash);
-  console.log( "\n");
+  console.log( '\n' );
   }
